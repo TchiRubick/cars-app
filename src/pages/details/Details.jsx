d7eceb4fd1fa68d1530a09dd62e5324c188c16fe
Tchi <tchi.devica@gmail.com>
2022-01-19 14:31:21 +0100

    fix: handle backend change

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import { AiFillMessage, AiOutlineUser } from 'react-icons/ai';
 
 import './Details.scss';
 
 import { getOne } from '../../api/cars.api';
 import Spinner from '../../kit/spinner/Spinner';
+import Button from '../../kit/button/Button';
 
 const Details = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const [data, setData] = useState(null);
+  const [carName, setCarName] = useState('');
 
   useEffect(() => {
     fetchDetails();
@@ -17,12 +22,23 @@ const Details = () => {
 
   const fetchDetails = async () => {
     const response = await getOne(id);
-    setData(response.comments);
+
+    const { contents, car } = response;
+
+    setData(contents);
+    setCarName(car);
   };
 
   const renderComment = () => data.map((d) => (
-    <div className="details-page__comment-text" key={d.id}>{d.content}</div>
+    <div className="details-page__comment-item" key={d.id}>
+      <div className="details-page__comment-text"><AiFillMessage />&nbsp;{d.comment}</div>
+      <div className="details-page__comment-name"><AiOutlineUser />{d.user}</div>
+    </div>
   ));
+
+  const handleClickBack = () => {
+    navigate('/');
+  };
 
   if (!data) {
     return <Spinner opened />;
@@ -30,10 +46,14 @@ const Details = () => {
 
   return (
     <div className="details-page">
-      <div className="details-page__name">{data.name}</div>
+      <div className="details-page__name">{carName}</div>
       <hr />
       <div className="details-page__comment">
         {renderComment()}
+      </div>
+      <hr />
+      <div className="details-page__button">
+        <Button onClick={handleClickBack}>Go back</Button>
       </div>
     </div>
   );
